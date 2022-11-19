a944ef9633b7cb9fa481a963ba75f57dbf19ddff
Rommel <rommel.valdiviezo@epn.edu.ec>
2022-11-19 01:53:53 -0500

    A- Noldbach problem

---
+++ Codeforces/Noldbach problem/a17.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define vi vector<int>
+#define ll long long
+#define vll vector<long long>
+#define fr(s,n,i) for (long long i=s; i<n; ++i)
+#define fri(s,e,i) for (long long i=s; i>=e; --i)
+#define len(arr) arr.size()
+#define viin(vec,n) fr(0,n,i){int x; cin>>x; vec.pb(x);}
+#define vllin(vec,n) fr(0,n,i){ll x; cin>>x; vec.pb(x);}
+using namespace std;
+
+
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int n,k;
+    cin >> n >> k;
+
+    int sieve[n+1];
+    fr(0,n+1,i){
+        sieve[i]=0;
+    }
+
+    fr(2,n+1,i){
+        if(sieve[i]) continue;
+        for(int j=2*i;j<=n;j+=i){
+            sieve[j]=1;
+        }
+    }
+
+    int left = 2;
+    int ans=0;
+    fr(3,n+1,i){
+        if (sieve[i]){
+            continue;
+        }else{
+            int suma = left + i + 1;
+            if(suma<=n){
+                if (sieve[suma]==0){
+                    ans++;
+                }
+            }
+            left = i;
+        }
+    }
+
+    if (k<=ans){
+        cout << "YES" << '\n';
+    }else{
+        cout << "NO" << '\n';
+    }
+
+
+    return 0;
+}
