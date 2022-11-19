8956ce9198b69c5c8880bc80b8c379e4bab84e6f
Rommel <rommel.valdiviezo@epn.edu.ec>
2022-11-19 01:53:27 -0500

    math algorithm

---
+++ Study/math/euclides.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define vi vector<int>
+#define ll long long
+#define vll vector<long long>
+#define fr(s,n,i) for (long long i=s; i<n; ++i)
+#define fri(s,e,i) for (long long i=s; i>=e; --i)
+#define len(arr) arr.size()
+#define viin(vec,n) fr(0,n,i){int x; cin>>x; vec.pb(x);}
+#define vllin(vec,n) fr(0,n,i){ll x; cin>>x; vec.pb(x);}
+using namespace std;
+
+int gcd(int a, int b){
+    if(b==0)
+        return a;
+    return gcd(b,a%b);
+}
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    int a=30,b=12;
+
+    cout << "GCD: "<<gcd(a,b) << '\n';
+    cout << "LCM: " << (a*b)/(gcd(a,b)) << '\n';
+
+    return 0;
+}
