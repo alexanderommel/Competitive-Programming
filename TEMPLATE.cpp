5f96ce5033afd8f8bee4bc7cca59411b20ca4cc9
Rommel <rommel.valdiviezo@epn.edu.ec>
2022-12-12 19:34:50 -0500

    Create TEMPLATE.cpp

---
+++ TEMPLATE.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define vi vector<int>
+#define ll long long
+#define vll vector<long long>
+#define fr(s,n,i) for (long long i=s; i<n; ++i)
+#define fri(s,e,i) for (long long i=s; i>=e; --i)
+#define len(arr) arr.size()
+#define viin(vec,n) fr(0,n,i){int x; cin>>x; vec.pb(x);}
+#define vllin(vec,n) fr(0,n,i){ll x; cin>>x; vec.pb(x);}
+using namespace std;
+
+typedef std::pair<ll,ll> mypair;
+bool my_comparator ( const mypair& l, const mypair& r){
+    return l.first > r.first;
+}
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    return 0;
+}
