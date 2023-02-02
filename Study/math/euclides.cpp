3ac81c053ca84b022512a28ebfb7d6f0b0f87fb1
Der <rommel.valdiviezo@epn.edu.ec>
2023-02-01 23:03:38 -0500

    Update euclides.cpp

--- Study/math/euclides.cpp
+++ Study/math/euclides.cpp
@@ -16,6 +16,10 @@ int gcd(int a, int b){
     return gcd(b,a%b);
 }
 
+int lcm(int a, int b){
+    return (a*b)/(gcd(a,b));
+}
+
 int main(){
     ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
