08ff5a557e19b8d9c587d3d674ae8a8994ea894b
Der <rommel.valdiviezo@epn.edu.ec>
2023-02-03 02:32:45 -0500

    priority queue y numeros voraces

--- Codeforces/Number Reduction/n1765.cpp
+++ Codeforces/Number Reduction/n1765.cpp
@@ -10,12 +10,88 @@
 #define vllin(vec,n) fr(0,n,i){ll x; cin>>x; vec.pb(x);}
 using namespace std;
 
-typedef std::pair<ll,ll> mypair;
-bool my_comparator ( const mypair& l, const mypair& r){
-    return l.first > r.first;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        int k;
+        cin >> k;
+        int p=0;
+        int p2=0;
+        bool worry = true;
+        vi ans;
+        vi leading;
+        priority_queue<pair<int,int>> pq; // second is index
+        while(p<s.size() && k>0){
+            //cout << "test p of: "<<p<<'\n';
+            int d = s[p]-'0';
+            while(!pq.empty() && k>0){
+                if(pq.size()==1 && d==0 && worry){
+                    break;
+                }
+                if(pq.top().first>d){
+                    if (pq.top().second<p2  && leading.size()>=k){
+                        pq.pop();
+                        continue;
+                    }
+                    //cout << "remove top value of " <<pq.top().first<<'\n';
+                    ans.pb(pq.top().second);
+                    k--;
+                    pq.pop();
+                    if(pq.empty()){
+                        if(worry){
+                            fr(0,leading.size(),xxx){
+                                ans.pb(leading[xxx]);
+                                k--;
+                            }
+                            leading.clear();
+                        }
+                    }
+                }else{
+                    break;
+                }
+            }
+            if(k==0){
+                break;
+            }
+            if(d!=0){
+                pq.push({d,p});
+            }else{
+                if(pq.size()>0 && worry){
+                    leading.pb(p);
+                    p2=p;
+                    //cout << "add leading zero\n"    ;
+                }
+            }
+            if(leading.size()>=k){
+                //cout << "end leadings "<<'\n';
+                worry=false;
+            }
+            p++;
+        }
+        std::sort(ans.begin(), ans.end());
+        string sANS = "";
+        fr(0,s.size(),i){
+            bool found = binary_search(ans.begin(),ans.end(),i);
+            if(!found){
+                sANS+=s[i];
+            }
+        }
+        fr(0,sANS.size()-k,i){
+            cout << sANS[i]<<"";
+        }
+        cout << '\n';
+    }
 }
 
-int main(){
+int main2(){
     ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
     int t;
