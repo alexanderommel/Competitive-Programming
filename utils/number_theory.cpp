0200e0b3b6cb310fcd5e1511a383ee331817549e
Der <rommel.valdiviezo@epn.edu.ec>
2023-02-28 01:21:18 -0500

    miller rabbin y descomposicion prima

--- utils/number_theory.cpp
+++ utils/number_theory.cpp
@@ -24,3 +24,64 @@ int sieve_of_eratosthenes(){
         }
     }
 }
+
+/** Descomposición prima sin repetición **/
+
+void unique_prime_factors_of_n(long long n) {
+    map<long long,long long> f;
+    for (long long x = 2; x*x <= n; x++) {
+        while (n%x == 0) {
+            if(f.count(x)<1){
+                f[x]++;
+            }
+            n /= x;
+        }
+    }
+    if (n > 1){
+        f[n]++;
+    }
+}
+
+/** Algoritmo de Miller Rabin para determinar si un número es probable primo **/
+long long binpower(long long base, long long e, long long mod) {
+    long long result = 1;
+    base %= mod;
+    while (e) {
+        if (e & 1)
+            result = (long long)result * base % mod;
+        base = (long long)base * base % mod;
+        e >>= 1;
+    }
+    return result;
+}
+
+bool check_composite(long long n, long long a, long long d, int s) {
+    long long x = binpower(a, d, n);
+    if (x == 1 || x == n - 1)
+        return false;
+    for (int r = 1; r < s; r++) {
+        x = (long long)x * x % n;
+        if (x == n - 1)
+            return false;
+    }
+    return true;
+};
+
+bool MillerRabin(long long n, int iter=5) { // returns true if n is probably prime
+    if (n < 4)
+        return n == 2 || n == 3;
+
+    int s = 0;
+    long long d = n - 1;
+    while ((d & 1) == 0) {
+        d >>= 1;
+        s++;
+    }
+
+    for (long long i = 0; i < iter; i++) {
+        int a = 2 + rand() % (n - 3);
+        if (check_composite(n, a, d, s))
+            return false;
+    }
+    return true;
+}
