2c6383e50305329d530f84ed041ce74fa2847b67
Rommel <rommel.valdiviezo@epn.edu.ec>
2023-06-24 17:31:56 -0500

    Create main.cpp

---
+++ Codeforces/Doremy's Paint/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+const ll L=1;
+const ll R = 1e13 +1;
+const int N=2*(1e5) + 1;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; ++i) {
+            ll asd;
+            cin >> asd;
+        }
+        cout << 1 <<" "<<n<<'\n';
+    }
+}
