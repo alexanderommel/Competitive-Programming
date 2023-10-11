27af4a3fdae3104e236db3d92be6fdce1b193425
Rommel <alexanderommelsw@outlook.es>
2023-10-11 12:12:49 -0500

    Update longest_paths_for_tree.cpp

--- Library/longest_paths_for_tree.cpp
+++ Library/longest_paths_for_tree.cpp
@@ -1,80 +1,68 @@
 #include <bits/stdc++.h>
 double PI = 3.14159265359;
 using namespace std;
+#define ll long long
 
 /**
  * ALL LONGEST PATHS FOR A UNDIRECTED TREE OF N-1 EDGES
  * THIS CODE COMPUTES THE LONGEST PATH FOR EACH NODE IN THE TREE
  */
 
-const int N = 100 + 1;
-bool visited[N];
-vector<int> adj[N];
-int DP[4][N];
+const int N = 200001;
 
-// STEP 1: Use the children nodes to compute the parents initial value
-int bfs1(int n){
-    visited[n]=true;
-    vector<pair<int,int>> max_paths(max(2,static_cast<int>(adj[n].size())),{0,n});
-    max_paths[0]={0,n};
-    max_paths[1]={0,n};
-    int i = -1;
-    for(auto c: adj[n]){
-        i++;
-        if (visited[c]) continue;
-        int my_son_path = 1+bfs1(c);
-        max_paths[i]={my_son_path,c};
-    }
-    std::sort(max_paths.begin(), max_paths.end());
-    std::reverse(max_paths.begin(), max_paths.end());
-    DP[0][n]=max_paths[0].first;
-    DP[1][n]=max_paths[0].second;
-    DP[2][n]=max_paths[1].first;
-    DP[3][n]=max_paths[1].second;
-    return DP[0][n];
-}
+vector<int> g[N];
+ll in[N], out[N];
 
-// Use parents to update children nodes value
-void bfs2(int n, int parent){
-    visited[n]=true;
-    vector<pair<int,int>> winners;
-    winners.emplace_back(DP[0][n],DP[1][n]);
-    winners.emplace_back(DP[2][n],DP[3][n]);
-    if (DP[1][parent]!=n) winners.emplace_back(1+DP[0][parent],parent);
-    if (DP[3][parent]!=n) winners.emplace_back(1+DP[2][parent],parent);
-    std::sort(winners.begin(), winners.end());
-    std::reverse(winners.begin(), winners.end());
-    DP[0][n]=winners[0].first;
-    DP[1][n]=winners[0].second;
-    DP[2][n]=winners[1].first;
-    DP[3][n]=winners[1].second;
-    for(auto c: adj[n]){
-        if (visited[c]) continue;
-        bfs2(c,n);
+// search childs
+ll dfs1(int n, int p){
+    in[n]=0LL;
+    for(auto c: g[n]){
+        if (c==p) continue;
+        dfs1(c,n);
+        in[n] = max(in[n],1+in[c]);
     }
 }
 
+// search on parent
+ll dfs2(int n, int p){
+    ll mx1 = -1;
+    ll mx2 = -1;
+    for(auto c: g[n]){
+        if (c==p) continue;
+        if (in[c]>=mx1) mx2 = mx1, mx1 = in[c];
+        else if (in[c]>mx2) mx2 = in[c];
+    }
+    for(auto c: g[n]){
+        if (c==p) continue;
+        ll use = mx1;
+        if (mx1 == in[c]) use = mx2;
+        out[c] = max(1LL + out[n],2LL + use);
+        dfs2(c,n);
+    }
+}
 
 int main(){
-    int n; cin >> n;
-    DP[0][0]=0;
-    DP[1][0]=1;
-    DP[2][0]=0;
-    DP[3][0]=1;
-    for (int i = 0; i < n + 1; ++i) {
-        visited[i]= false;
-    }
-    for (int i = 0; i < n-1; ++i) {
-        int a,b; cin >> a >> b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
-    bfs1(1);
-    for (int i = 0; i < n + 1; ++i) {
-        visited[i]= false;
-    }
-    bfs2(1,0);
-    for (int i = 0; i < n+1; ++i) {
-        cout << DP[0][i]<<" ";
+
+    int t; cin >> t;
+    while(t--){
+        int n; long long k,c; cin >> n >> k >> c;
+        for (int i = 0; i < n + 1; ++i) {
+            g[i].clear();
+            in[i]=0;
+            out[i]=0;
+        }
+        for (int i = 0; i < n-1; ++i) {
+            int a,b; cin >> a >> b;
+            g[a].push_back(b);
+            g[b].push_back(a);
+        }
+        dfs1(1,0);
+        dfs2(1,0);
+        ll ans = 0;
+        for (int i = 1; i <=n ; ++i) {
+            ll max_distance = max(in[i],out[i]);
+            ans = max(ans,max_distance);
+        }
+        cout << ans <<'\n';
     }
 }
